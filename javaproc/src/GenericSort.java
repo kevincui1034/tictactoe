655a51b6e3004c2a5e408c0c69a23195fbee290d
Kevin <93696112+kevincui1034@users.noreply.github.com>
2021-12-10 00:06:56 -0800

    Update GenericSort.java

--- javaproc/src/GenericSort.java
+++ javaproc/src/GenericSort.java
@@ -75,5 +75,26 @@ public class GenericSort {
             System.out.print(arr[i]+" ");
         } 
     } 
+    
+    void reverse(int[] array)
+    {
+  
+        // Length of the array
+        int n = array.length;
+  
+        // Swaping the first half elements with last half
+        // elements
+        for (int i = 0; i < n / 2; i++) {
+  
+            // Storing the first half elements temporarily
+            int temp = array[i];
+  
+            // Assigning the first half to the last half
+            array[i] = array[n - i - 1];
+  
+            // Assigning the last half to the first half
+            array[n - i - 1] = temp;
+        }
+    }
 }
 
